fc890d3cdef63b7c75ee30acbbd9fd973c812501
orlandoag89 <orlandoag89@gmail.com>
2026-04-02 17:23:38 -0600

    se agrega clase main

--- src/main/java/com/blo/sales/v2/AppMain.java
+++ src/main/java/com/blo/sales/v2/AppMain.java
@@ -6,20 +6,20 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class AppMain {
-    
+
     public static void main(String[] args) {
         try {
-        Injector injector = Guice.createInjector(new BloSalesV2SingletonConfig());
-        java.awt.EventQueue.invokeLater(() -> {
-            LoginFrm login = injector.getInstance(LoginFrm.class);
-            login.setVisible(true);
-        });
-    } catch (Exception e) {
-        // ESTO TE DIRÁ EXACTAMENTE QUÉ PIEZA FALTA
-        System.err.println("--- ERROR DE CONFIGURACIÓN DE GUICE ---");
-        System.err.println(e.getMessage()); 
-        System.exit(1);
+            Injector injector = Guice.createInjector(new BloSalesV2SingletonConfig());
+            java.awt.EventQueue.invokeLater(() -> {
+                LoginFrm login = injector.getInstance(LoginFrm.class);
+                login.setVisible(true);
+            });
+        } catch (Exception e) {
+            // ESTO TE DIRÁ EXACTAMENTE QUÉ PIEZA FALTA
+            System.err.println("--- ERROR DE CONFIGURACIÓN DE GUICE ---");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
-    }
-    
+
 }
